466dfb009d850686b06a2d567fdfd6108e09db87
Artem <selfex9496@gmail.com>
2023-10-01 13:52:20 +0700

    eslint fixes

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { initReactI18next } from 'react-i18next';
-import i18next from 'i18next';
+// import { initReactI18next } from 'react-i18next';
+// import i18next from 'i18next';
 // import LanguageDetector from 'i18next-browser-languagedetector';
 import App from './components/App.jsx';
-import resources from './locales/index.js';
+// import resources from './locales/index.js';
 import reportWebVitals from './reportWebVitals.js';
 
 const runApp = async () => {
-  const i18n = i18next.createInstance();
+  // const i18n = i18next.createInstance();
 
-  await i18n
-    .use(initReactI18next)
-    // .use(LanguageDetector)
-    .init({
-      resources,
-      fallbackLng: 'ru',
-      interpolation: {
-        escapeValue: false,
-      },
-    });
+  // await i18n
+  //   .use(initReactI18next)
+  //   // .use(LanguageDetector)
+  //   .init({
+  //     resources,
+  //     fallbackLng: 'ru',
+  //     interpolation: {
+  //       escapeValue: false,
+  //     },
+  //   });
 
   const mountNode = document.getElementById('root');
   const root = ReactDOM.createRoot(mountNode);
@@ -27,7 +27,7 @@ const runApp = async () => {
   root.render(
     <React.StrictMode>
       <App />
-    </React.StrictMode>
+    </React.StrictMode>,
   );
 };
 
